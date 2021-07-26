7ee937120fccdc8f5f500cc0b987548a5c0a30d1
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-07-26 20:16:46 +0100

    Collaborated fix for Holberton.h file

--- holberton.h
+++ holberton.h
@@ -3,28 +3,27 @@
 
 #include <stdarg.h>
 #include <string.h>
+
 /**
- *  * struct printer - A new struct type defining a printer
- *   * @symbol: A symbol representing a data type
- *    * @print: A function pointer to a function that prints a data type corresponding to symbol
- *     *
- *      */
+ * struct printer - A new struct type defining a printer
+ * @symbol: A symbol representing a data type
+ * @print: A pointer to that prints a data type corresponding to symbol
+ *
+ */
+
+
 typedef struct printer
 {
-		char *symbol;
-		void (*print)(va_list arg);
-
+char *symbol;
+void (*print)(va_list *arg);
 } printer_t;
-
-
-
 
 int _putchar(char);
 int _printf(const char *format, ...);
-void print_char(va_list arg);
-void print_string(va_list arg);
-void print_percent(va_list arg);
-void print_break(va_list arg);
+void print_char(va_list *arg);
+void print_string(va_list *arg);
+void print_percent(va_list *arg);
+void print_break(va_list *arg);
 int get_op(const char s, void *args);
 
 #endif
