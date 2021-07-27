7deaf1861f863ec66532512c0d3bae9a9c692dfe
HenryIyke <realhenryiyke@gmail.com>
2021-07-27 02:51:57 +0100

    recent commit

--- functions_1.c
+++ functions_1.c
@@ -1,6 +1,8 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include "holberton.h"
+#include <string.h>
+
 /**
  * convert_fmt_percent - Prints a percent sign (%)
  * @args_list: The arguments list
@@ -10,10 +12,11 @@
  */
 int convert_fmt_percent(va_list *args_list, fmt_info_t *fmt_info)
 {
-(void)args_list;
-_putchar(fmt_info->spec);
-return (1);
+  (void)args_list;
+  _putchar(fmt_info->spec);
+  return (1);
 }
+
 /**
  * convert_fmt_p - Prints the pointer address
  * @args_list: The arguments list
@@ -23,35 +26,37 @@ return (1);
  */
 int convert_fmt_p(va_list *args_list, fmt_info_t *fmt_info)
 {
-int i, chars_count = 0, size = 16;
-size_t num = va_arg(*args_list, size_t), tmp;
-char *str;
-(void)fmt_info;
-(void)args_list;
-str = malloc(sizeof(char) * (size));
-if (str)
-{
-mem_set(str, 16, '0');
-tmp = num;
-for (i = 0; i <= size && tmp > 0; i++)
-{
-*(str + i) = (tmp % 16) < 10 ? (tmp % 16) + '0'
-: (tmp % 16) - 10 + 'a';
-tmp /= 16;
+  int i, chars_count = 0, size = 16;
+  size_t num = va_arg(*args_list, size_t), tmp;
+  char *str;
+
+  (void)fmt_info;
+  (void)args_list;
+  str = malloc(sizeof(char) * (size));
+  if (str)
+    {
+      mem_set(str, 16, '0');
+      tmp = num;
+      for (i = 0; i <= size && tmp > 0; i++)
+	{
+	  *(str + i) = (tmp % 16) < 10 ? (tmp % 16) + '0'
+	    : (tmp % 16) - 10 + 'a';
+	  tmp /= 16;
+	}
+      rev_string(str);
+      str = trim_start(str, '0', TRUE);
+      _putstr("0x");
+      chars_count += 2;
+      for (i = 0; i < size; i++)
+	{
+	  _putchar(*(str + i));
+	  chars_count++;
+	}
+      free(str);
+    }
+  return (chars_count);
 }
-rev_string(str);
-str = trim_start(str, '0', TRUE);
-_putstr("0x");
-chars_count += 2;
-for (i = 0; i < size; i++)
-{
-_putchar(*(str + i));
-chars_count++;
-}
-free(str);
-}
-return (chars_count);
-}
+
 /**
  * convert_fmt_c - Prints a character
  * @args_list: The arguments list
@@ -61,22 +66,24 @@ return (chars_count);
  */
 int convert_fmt_c(va_list *args_list, fmt_info_t *fmt_info)
 {
-int i, chars_count = 0, len = 1;
-char str = va_arg(*args_list, int);
-if (!fmt_info->left)
-{
-for (i = 0; i < MAX(len, fmt_info->width) - len; i++)
-_putchar(fmt_info->pad);
+  int i, chars_count = 0, len = 1;
+  char str = va_arg(*args_list, int);
+
+  if (!fmt_info->left)
+    {
+      for (i = 0; i < MAX(len, fmt_info->width) - len; i++)
+	_putchar(fmt_info->pad);
+    }
+  _putchar(str == '\0' ? ' ' : str);
+  chars_count += MAX(len, fmt_info->width) - len + 1;
+  if (fmt_info->left)
+    {
+      for (i = 0; i < MAX(len, fmt_info->width) - len; i++)
+	_putchar(' ');
+    }
+  return (chars_count);
 }
-_putchar(str == '\0' ? ' ' : str);
-chars_count += MAX(len, fmt_info->width) - len + 1;
-if (fmt_info->left)
-{
-for (i = 0; i < MAX(len, fmt_info->width) - len; i++)
-_putchar(' ');
-}
-return (chars_count);
-}
+
 /**
  * convert_fmt_s - Prints a string
  * @args_list: The arguments list
@@ -86,24 +93,24 @@ return (chars_count);
  */
 int convert_fmt_s(va_list *args_list, fmt_info_t *fmt_info)
 {
-int i, chars_count = 0, len;
-char *str = va_arg(*args_list, char *);
-char null_str[] = "(null)";
-str = str ? str : null_str;
-len = str_len(str);
-if (!fmt_info->left)
-{
-for (i = 0; i < MAX(len, fmt_info->width) - len; i++)
-_putchar(fmt_info->pad);
-}
-}
-chars_count += MAX(len, fmt_info->width) - len;
-_putstr(str);
-chars_count += str_len(str);
-if (fmt_info->left)
-{
-for (i = 0; i < MAX(len, fmt_info->width) - len; i++)
-_putchar(' ');
-}
-return (chars_count);
+  int i, chars_count = 0, len;
+  char *str = va_arg(*args_list, char *);
+  char null_str[] = "(null)";
+
+  str = str ? str : null_str;
+  len = str_len(str);
+  if (!fmt_info->left)
+    {
+      for (i = 0; i < MAX(len, fmt_info->width) - len; i++)
+	_putchar(fmt_info->pad);
+    }
+  chars_count += MAX(len, fmt_info->width) - len;
+  _putstr(str);
+  chars_count += str_len(str);
+  if (fmt_info->left)
+    {
+      for (i = 0; i < MAX(len, fmt_info->width) - len; i++)
+	_putchar(' ');
+    }
+  return (chars_count);
 }
